82ba9f0de4344d4fd506f5dbd2ac9bb033f01725
Arfin Dwi Octavianto <arfindwioctavianto@gmail.com>
2023-12-30 14:30:30 +0700

    feat: add styling in edit-article.php

--- admin/create-article.php
+++ admin/create-article.php
@@ -204,9 +204,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         <!-- Main Section End -->
     </div>
 
-
-
-
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 
 </html>
